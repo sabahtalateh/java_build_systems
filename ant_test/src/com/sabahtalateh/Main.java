b8d6631d12da7e276b0574604dc3e47f76887b88
Alexandr Kravtsov <ak@4xxi.com>
2017-06-16 13:37:22 +0300

    WIP

---
+++ ant_test/src/com/sabahtalateh/Main.java
@@ -0,0 +1,16 @@
+package com.sabahtalateh;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame fr = new JFrame("Frame");
+        fr.setSize(new Dimension(800, 600));
+        fr.setLocale(null);
+        fr.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        fr.setVisible(true);
+    }
+}
---
+++ ant_test/src/com/sabahtalateh/a/A.java
@@ -0,0 +1,4 @@
+package com.sabahtalateh.a;
+
+public class A {
+}
